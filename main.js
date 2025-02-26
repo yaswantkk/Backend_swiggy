1b5493d015874fe1c35fd0e980e4d35b50a13207
Yaswantkk <yaswanthsaigunji.com>
2025-02-26 17:27:13 +0530

    change1

--- main.js
+++ main.js
@@ -7,13 +7,15 @@ const bodyparser=require('body-parser')
 const firmroutes=require('./routes/firmroutes')
 const productroutes=require('./routes/productRoutes')
 const path =require('path')
-const port=process.env.port || 5000
+const port=process.env.port || 5000;
+const cors=require('cors')
 
 dotenv.config();
 mongoose.connect(process.env.mongo_uri)
 .then(()=>{console.log("mongodb connected successfully")})
 .catch((err)=>{console.log(err)})
 
+app.use(cors())
 app.use(bodyparser.json())
 app.use('/vendor',vendorRoutes)
 app.use('/firm',firmroutes)
